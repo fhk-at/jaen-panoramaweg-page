2f2cdb7c8311e664af0eb87eccac82aad058a3cd
petute <daniel.petutschnigg@gmail.com>
2021-09-11 17:50:50 +0200

    chore: add HeroHidden

---
+++ src/components/molecules/HeroHidden/index.tsx
@@ -0,0 +1,22 @@
+import {Box} from '@chakra-ui/react'
+import {useOptions} from '@snek-at/jaen-pages'
+import {withRedux} from '@snek-at/jaen-pages'
+
+const HeroHidden = () => {
+  const {isEditing} = useOptions()
+
+  return (
+    <Box
+      display={isEditing ? 'none' : 'absolute'}
+      bg="#9C27B01A"
+      width="100vw"
+      height="100vh"
+      position="absolute"
+      top="0"
+      left="0"
+      zIndex="5"
+    />
+  )
+}
+
+export default withRedux(HeroHidden)
